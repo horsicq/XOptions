6b31f42879b6ad94853856663415d3495e64d8e0
horsicq <horsicq@gmail.com>
2022-11-21 01:18:16 +0100

    Fix: 2022-11-21

--- xoptions.h
+++ xoptions.h
@@ -62,8 +62,9 @@ class XOptions : public QObject {
 public:
     // TODO Check if update available
     // TODO Show in dialog all available updates
-    // Ubuntu -> Ubuntu+Debian+appImage
-    // Debian -> Debian+appImage
+    // Ubuntu -> Ubuntu + Debian + appImage
+    // Debian -> Debian + appImage
+    // Parrot -> Parrot + Debian + appImage
     // Windows 64 -> Windows 32 + Windows 64 + Windows XP
     // Windows 32 -> Windows 32 + Windows XP
     // Windows XP -> Windows XP
