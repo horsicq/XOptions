ecf1baaa70cb77821e08e94402746f72f936479f
horsicq <horsicq@gmail.com>
2024-03-01 00:08:23 +0100

    Update file(s): 2024-03-01

--- xoptions.cmake
+++ xoptions.cmake
@@ -2,5 +2,7 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(XOPTIONS_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/xoptions.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xoptions.h
     ${CMAKE_CURRENT_LIST_DIR}/codecs/codec_cp437.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/codecs/codec_cp437.h
 )
--- xoptionswidget.cmake
+++ xoptionswidget.cmake
@@ -5,7 +5,9 @@ include(${CMAKE_CURRENT_LIST_DIR}/xoptions.cmake)
 set(XOPTIONSWIDGET_SOURCES
     ${XOPTIONS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xoptionswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xoptionswidget.h
     ${CMAKE_CURRENT_LIST_DIR}/xoptionswidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogviewcolors.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogviewcolors.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogviewcolors.ui
 )
