ef4c4ff83d2fcb35ee2cc7b9668c63e0a87d180b
horsicq <horsicq@gmail.com>
2024-03-21 18:59:33 +0100

    Update file(s): 2024-03-21

--- xoptions.cpp
+++ xoptions.cpp
@@ -105,6 +105,7 @@ XOptions::GROUPID XOptions::getGroupID(ID id)
         case ID_SCAN_HEURISTIC:
         case ID_SCAN_VERBOSE:
         case ID_SCAN_ALLTYPES:
+        case ID_SCAN_HIGHLIGHT:
         case ID_SCAN_ENGINE:
         case ID_SCAN_ENGINE_EMPTY:
         case ID_SCAN_DATABASEPATH:
@@ -438,6 +439,7 @@ QString XOptions::idToString(ID id)
         case ID_SCAN_HEURISTIC: sResult = QString("Scan/Heuristic"); break;
         case ID_SCAN_VERBOSE: sResult = QString("Scan/Verbose"); break;
         case ID_SCAN_ALLTYPES: sResult = QString("Scan/AllTypes"); break;
+        case ID_SCAN_HIGHLIGHT: sResult = QString("Scan/Highlight"); break;
         case ID_SCAN_ENGINE:
         case ID_SCAN_ENGINE_EMPTY: sResult = QString("Scan/Engine"); break;
         case ID_SCAN_DATABASEPATH: sResult = QString("Scan/DatabasePath"); break;
