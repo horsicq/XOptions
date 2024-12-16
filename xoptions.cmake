a319f17dd882c37f38a84671b48ab35a1f74c8ae
horsicq <horsicq@gmail.com>
2024-12-17 00:52:08 +0100

    Update file(s): 2024-12-17

--- xoptions.cmake
+++ xoptions.cmake
@@ -1,6 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(XOPTIONS_SOURCES
+    ${XOPTIONS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xoptions.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xoptions.h
     ${CMAKE_CURRENT_LIST_DIR}/codecs/codec_cp437.cpp
