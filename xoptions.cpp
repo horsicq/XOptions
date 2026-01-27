b6aa22448af04b9babce669583b505ae7340e1b2
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2026-01-27 19:44:25 +0100

    docs: add TrID todo comment in xoptions.cpp

--- xoptions.cpp
+++ xoptions.cpp
@@ -1118,6 +1118,7 @@ void XOptions::setComboBox(QComboBox *pComboBox, XOptions::ID id)
         pComboBox->addItem(QString("Detect It Easy (DiE)"), "die");
         pComboBox->addItem(QString("Nauz File Detector (NFD)"), "nfd");
         // TODO: peid
+        // TODO: TrID
 #ifdef USE_YARA
         pComboBox->addItem(QString("Yara rules"), "yara");
 #endif
